853bcd777fceb56599b141ed992f1a9f39e77365
Zeynep Kıcıkoğlu <zeynep.kicikoglu2408@gmail.com>
2023-10-26 19:30:54 +0300

    commit

---
+++ if-else/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Conditions
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+
+            // bool condition = true;
+
+            // if (condition)
+            // {
+            //     Console.WriteLine("Değişken: true");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Değişken: false");
+            // }
+
+			int time = DateTime.Now.Hour;
+			if (time >= 6 && time < 11)
+			{
+				Console.WriteLine("Günaydın!");
+			}
+			else if (time <= 18)
+			{
+				Console.WriteLine("İyi Günler!");
+			}
+			else
+			{ 
+			Console.WriteLine("İyi Geceler!");
+            
+			string sonuc = time <= 18 ? "İyi Günler!" : "İyi Geceler!";
+			sonuc = time >= 6 && time < 11 ? "Günaydın!" : time <= 18 ? "İyi Günler!" : "İyi Geceler!";
+			Console.WriteLine(sonuc);
+			}
+		}
+	}
+}
